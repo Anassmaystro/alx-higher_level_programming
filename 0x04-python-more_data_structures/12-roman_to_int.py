8f851e2d566c2d194d39d17f1453f03f42f6f754
Anassmaystro <sendi.anass97@gmail.com>
2023-06-14 08:24:34 -0700

    Roman to Integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string or not isinstance(roman_string, str):
+        return 0
+    su = 0
+    vald = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    for i in range(len(roman_string)):
+        if roman_string[i] not in vald:
+            return 0
+        if i == len(roman_string) - 1:
+            su += vald[roman_string[i]]
+        else:
+            if vald[roman_string[i]] < vald[roman_string[i + 1]]:
+                su -= vald[roman_string[i]]
+            else:
+                su += vald[roman_string[i]]
+    return su
