da3fd7c7c831698bea0fe2c99056457ec47a6192
Anassmaystro <sendi.anass97@gmail.com>
2023-08-15 15:03:27 -0700

    List by best

--- 10-top_score.sql
+++ 10-top_score.sql
@@ -1,4 +1,4 @@
--- Lists all records of the table
-SELECT score, name
-FROM second_table
-ORDER BY score DESC;
+-- Lists all records of the table second_table.
+SELECT `score`, `name`
+FROM `second_table`
+ORDER BY `score` DESC;
