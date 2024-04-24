bb3a02224ceeddd740bf80aa2630221ce85977a6
Anassmaystro <sendi.anass97@gmail.com>
2024-04-23 17:35:41 -0700

    11. Factor index

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
-const newList = list.map((ele, index) => ele * index);
+
+const newList = list.map(function (value, index) {
+  return value * index;
+});
+
 console.log(list);
 console.log(newList);
